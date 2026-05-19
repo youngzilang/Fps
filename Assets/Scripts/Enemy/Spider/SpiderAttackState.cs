3180d2c8ee5b6cdd3ecf211243cd2e2c44f55e60
3123004375-杨子朗 <1301741765@qq.com>
2026-05-20 01:29:53 +0800

    剩下NavmeshAgent和rigidbody之间的冲突需解决

--- Assets/Scripts/Enemy/Spider/SpiderAttackState.cs
+++ Assets/Scripts/Enemy/Spider/SpiderAttackState.cs
@@ -7,8 +7,6 @@ public class SpiderAttackState : EnemyState
 {
     protected Spider spider;
 
-    private float attackTimer;
-    private float attackCD=1f;
 
     public SpiderAttackState(Enemy _enemy, EnemyStateMachine _stateMachine, string _aniName,Spider _spider) : base(_enemy, _stateMachine, _aniName)
     {
@@ -20,7 +18,7 @@ public class SpiderAttackState : EnemyState
         base.Enter();
         //在攻击状态进入时，停止移动
         spider.StopMoving();
-        attackTimer=attackCD; //重置攻击计时器
+     
     }
 
     public override void Update()
@@ -32,18 +30,7 @@ public class SpiderAttackState : EnemyState
             stateMachine.ChangeState(spider.walkState);
             return;
         }
-        //如果玩家在攻击范围内，继续攻击
-        attackTimer+=Time.deltaTime;
-        if(attackTimer>=attackCD)
-        {
-            attackTimer=0f; //重置攻击计时器
-            ReAttack();//再次攻击
-        }
-    }
-
-    private void ReAttack()
-    {
-        
+      
     }
 
     public override void Exit() 
